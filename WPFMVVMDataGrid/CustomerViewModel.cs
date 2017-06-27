25ac6d283ee58abb6d04e8723801a74b7b5696f0
dotnetgenetics <gregory_keyboard[@]outlook.com>
2017-06-27 18:14:30 +0800

    Initial Commit

---
+++ WPFMVVMDataGrid/CustomerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMDataGrid
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+        private ObservableCollection<Customer> _cusGridData;
+        private NorthwindEntities _context;
+
+        public CustomerViewModel()
+        {
+            _context = new NorthwindEntities();
+            LoadInitialData();
+        }
+
+        private void LoadInitialData(){
+            CustomerData = new ObservableCollection<Customer>(from c in _context.Customers select c);
+        }
+
+        public ObservableCollection<Customer> CustomerData
+        {
+            get 
+            { 
+                return _cusGridData; 
+            }
+            set
+            {
+                _cusGridData = value;
+                OnPropertyChanged("CustomerData");
+            }
+        }
+    }
+}
